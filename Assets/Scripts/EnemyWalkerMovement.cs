5826216e9ee48dfe0d3ebe016168b9149c1259f9
Alexander LeBlanc <ehardor@gmail.com>
2023-10-21 16:56:59 -0300

    Enemy AI finished

--- Assets/Scripts/EnemyWalkerMovement.cs
+++ Assets/Scripts/EnemyWalkerMovement.cs
@@ -20,12 +20,15 @@ public class EnemyWalkerMovement : MonoBehaviour
     private float _rightBounds;
     private float _leftBounds;
     [SerializeField] private float _pathOffset = 3f;
-    [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _speed = 80f;
     private float _walkDirection = 1f;
 
-    [SerializeField] private float _aggroRange = 2f;
-    [SerializeField] private float _deaggroRange = 3f;
+    [SerializeField] private float _aggroRange = 10f;
+    [SerializeField] private float _deaggroRange = 15f;
+    [SerializeField] private float _aggroSpeed = 120f;
     private bool _isAggro = false;
+
+    private float _curSpeed = 0f;
 
 
     // Start is called before the first frame update
@@ -44,7 +47,7 @@ public class EnemyWalkerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (!_isAggro && Vector3.Distance(transform.position, _player.transform.position) <= _aggroRange)
         {
@@ -57,38 +60,64 @@ public class EnemyWalkerMovement : MonoBehaviour
         //If aggro move towards character
         //If not aggro move either back to move range, or move in range
 
-        if (_stateChangeTimer <= Time.time) {
-            if (_isWalking)
+        if (!_isAggro && transform.position.x > _leftBounds && transform.position.x < _rightBounds)
+        {
+            if (_stateChangeTimer <= Time.time)
             {
-                _isIdle = true;
-                _isWalking = false;
-                GetComponent<Animator>().SetBool("IsWalking", false);
-                _stateChangeTimer = Time.time + Random.Range(0.0f, 1.5f);
-                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-            }
-            else if (_isIdle)
-            {
-                _isWalking = true;
-                _isIdle = false;
-                GetComponent<Animator>().SetBool("IsWalking", true);
+                if (_isWalking)
+                {
+                    _isIdle = true;
+                    _isWalking = false;
+                    GetComponent<Animator>().SetBool("IsWalking", false);
+                    _stateChangeTimer = Time.time + Random.Range(0.0f, 1.5f);
+                    _curSpeed = 0;
+                }
+                else if (_isIdle)
+                {
+                    _isWalking = true;
+                    _isIdle = false;
+                    GetComponent<Animator>().SetBool("IsWalking", true);
 
-                if (transform.position.x >= _rightBounds)
-                {
-                    _walkDirection = -1f;
-                }
-                else if (transform.position.x <= _leftBounds)
-                {
-                    _walkDirection = 1f;
-                }
-                else
-                {
-                    _walkDirection = (Random.Range(0, 2) == 0) ? 1 : -1;
-                }
+                    if (transform.position.x >= _rightBounds)
+                    {
+                        _walkDirection = -1f;
+                    }
+                    else if (transform.position.x <= _leftBounds)
+                    {
+                        _walkDirection = 1f;
+                    }
+                    else
+                    {
+                        _walkDirection = (Random.Range(0, 2) == 0) ? 1 : -1;
+                    }
 
-                _stateChangeTimer = Time.time + Random.Range(1.0f, 3.0f);
-                GetComponent<Rigidbody2D>().velocity = new Vector2(_speed * _walkDirection, 0);
+                    _stateChangeTimer = Time.time + Random.Range(1.0f, 3.0f);
+                    _curSpeed = _speed * _walkDirection;
+                }
             }
         }
+        else if (!_isAggro && (transform.position.x < _leftBounds || transform.position.x > _rightBounds))
+        {
+            if (transform.position.x < _leftBounds && (_rigidbody.velocity.x / Mathf.Abs(_rigidbody.velocity.x)) != 1f)
+            {
+                _curSpeed = _speed;
+            }
+            else if (transform.position.x > _rightBounds && (_rigidbody.velocity.x / Mathf.Abs(_rigidbody.velocity.x)) != -1f)
+            {
+                _curSpeed = _speed * -1f;
+            }
+        }
+        else if (_isAggro) {
+            Debug.Log("Aggroed");
+            Vector3 dir = _player.transform.position - transform.position;
+            if ((_rigidbody.velocity.x / Mathf.Abs(_rigidbody.velocity.x)) != (dir.x / Mathf.Abs(dir.x))) {
+                _curSpeed = _aggroSpeed * (dir.x / Mathf.Abs(dir.x));
+            }
+        }
+
+        var velocity = _rigidbody.velocity;
+        velocity.x = _curSpeed * Time.deltaTime;
+        _rigidbody.velocity = velocity;
 
 
         //FlipDir
