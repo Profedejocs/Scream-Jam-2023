460fcdf2036a0c5ac2f3e86578c7ff757499d1f9
Alexander LeBlanc <ehardor@gmail.com>
2023-10-23 20:14:10 -0300

    saving

--- Assets/MusicFadeTrigger.cs
+++ Assets/MusicFadeTrigger.cs
@@ -11,7 +11,36 @@ public class MusicFadeTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!_isTriggered && collision.gameObject.name.Equals("Character")) { 
-            
+            _isTriggered = true;
+            var sources = collision.gameObject.GetComponents<AudioSource>();
+
+            foreach (var source in sources)
+            {
+                if (source.clip.name.Equals("Scream Jam SFX Master ambience_forest_night"))
+                {
+                    _charSource1 = source;
+                }
+                else if (source.clip.name.Equals("Monster Ambience, Distant Destruction, Huge Groans")) { 
+                    _charSource2 = source;
+                }
+            }
+
+            StartCoroutine(FadeMusic());
         }
+    }
+
+    private IEnumerator FadeMusic() {
+        float currentTime = 0;
+        float duration = 3f;
+        float start1 = _charSource1.volume;
+        float start2 = _charSource2.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            _charSource1.volume = Mathf.Lerp(start1, 0, currentTime / duration);
+            _charSource2.volume = Mathf.Lerp(start2, 0, currentTime / duration);
+            yield return null;
+        }
+        yield break;
     }
 }
